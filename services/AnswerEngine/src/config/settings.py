a3d0e82275f2b80e8c9528da44fe90aba8ab4c7e
Кирилл Горюнов <kirillgorunov@MacBook-Pro-Kirill.local>
2025-05-11 01:45:18 +0300

    add 3000 delay schedule

--- services/AnswerEngine/src/config/settings.py
+++ services/AnswerEngine/src/config/settings.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     MQPASSWORD: str = os.getenv('RABBITMQ_PASSWORD')
     MQHOST: str = os.getenv('RABBITMQ_HOST')
     MQPORT: int = int(os.getenv('RABBITMQ_PORT'))
-    DELAY_SCHEDULE: int = os.getenv('DELAY_SCHEDULE')
+    DELAY_SCHEDULE: int = os.getenv('DELAY_SCHEDULE', "3000")
     SUPPORT_CHANNEL: str = os.getenv('SUPPORT_CHANNEL')
 
     def get_webhook_url(self) -> str:
